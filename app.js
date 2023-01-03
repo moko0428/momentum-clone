1918afb70c3e891ca8cb296e15938dcb0b527a6b
Leejunyoung <39412219+moko0428@users.noreply.github.com>
2023-01-04 03:53:35 +0900

    이름이 입력되면 form 없애고 인삿말 출력하기

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
 // 로그인폼
 const loginForm = document.getElementById("login-form");
 const loginInput = loginForm.querySelector("input");
+const greeting = document.querySelector("#greeting");
 
-const link = document.querySelector("a");
+const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event) {
   event.preventDefault();
-  console.log(loginInput.value);
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  greeting.innerText = `Hello ${username}!`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-function linkClick(event) {
-  event.preventDefault();
-  console.dir(event);
-}
 loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", linkClick);
