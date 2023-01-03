c3ded3994a5d686c662c105f3997e23a7bef7d73
Leejunyoung <39412219+moko0428@users.noreply.github.com>
2023-01-04 03:38:39 +0900

    preventDefault()에 대한 이해

--- app.js
+++ app.js
@@ -2,9 +2,16 @@
 const loginForm = document.getElementById("login-form");
 const loginInput = loginForm.querySelector("input");
 
+const link = document.querySelector("a");
+
 function onLoginSubmit(event) {
   event.preventDefault();
   console.log(loginInput.value);
 }
 
+function linkClick(event) {
+  event.preventDefault();
+  console.dir(event);
+}
 loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", linkClick);
